4fac83390067375190c08723365997aa5e7f5340
KaipoJames <kaiposemail@yahoo.com>
2020-12-08 00:04:12 -1000

    Add placeholder code for rest of the components

---
+++ exercise-tracker/src/components/create-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from ʻreactʻ;
+
+export default class CreateExercise extends Component {
+    render() {
+        return (
+            <div>
+                <p>This is the Create Exercise Component</p>
+            </div>
+        )
+    }
+}
---
+++ exercise-tracker/src/components/edit-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from ʻreactʻ;
+
+export default class EditExercise extends Component {
+    render() {
+        return (
+            <div>
+                <p>This is the Edit Exercise Component</p>
+            </div>
+        )
+    }
+}
